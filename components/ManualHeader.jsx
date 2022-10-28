3a633f193cad2b17c5b3d786a9747de87341e775
BowTiedApu <114339731+bowtiedapu@users.noreply.github.com>
2022-10-27 20:33:31 -0400

    feat: Adding basic async function to await the enableWeb3() function call when using react moralis

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -3,8 +3,12 @@ import {useMoralis} from "react-moralis"
 export default function ManualHeader() {
     // This acts as our React hook, allowing us to latch onto the React state and lifecycle features
     const {enableWeb3} = useMoralis() 
+    // The whole point of using hooks like this is so that our front end can re-render whenever the state of the React app changes
+
 
     // To use Moralis correctly, we need to create a provider in our _app.js
     
-    return(<div>Henlo world from header</div>)
+    return(<div>
+        <button onClick={async () => {await enableWeb3()}}>Connect</button>
+        </div>)
 }
