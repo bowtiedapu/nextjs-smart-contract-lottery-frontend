bf10ca4afef605b565301cfe5c26987b2fdb77d6
BowTiedApu <114339731+bowtiedapu@users.noreply.github.com>
2022-10-28 23:11:10 -0400

    feat: Adding on isWeb3EnableLoading

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -2,7 +2,7 @@ import {useMoralis} from "react-moralis"
 import {useEffect} from "react"
 export default function ManualHeader() {
     // This acts as our React hook, allowing us to latch onto the React state and lifecycle features
-    const {enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3} = useMoralis() 
+    const {enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading} = useMoralis() 
     // The whole point of using hooks like this is so that our front end can re-render whenever the state of the React app changes
 
 
@@ -51,8 +51,9 @@ export default function ManualHeader() {
                 if (typeof window !== "undefined") {
                     window.localStorage.setItem("connected", "inject")
                 }
-                }
-                }>
+                }}
+                disabled={isWeb3EnableLoading}
+                >
                     Connect
             </button>)
             }
